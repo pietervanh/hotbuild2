8592fb761b5234dfc10081838bd8f0dd6491f969
Pieter Vanhoutteghem <pieter.vanh@gmail.com>
2013-12-11 13:09:11 +0100

    Update hotbuild_settings.js

--- settings/hotbuild_settings.js
+++ settings/hotbuild_settings.js
@@ -1,3 +1,3 @@
-model.addSetting_DropDown('Hotbuild Preview UI','hotbuild_preview_display','UI',['ON','OFF'],0);
-model.addSetting_Text('Hotbuild Reset Time','hotbuild_reset_time','UI','Number',2000);
-model.addSetting_Text('Hotbuild Requeue Amount','hotbuild_requeue_amount','UI','Number',50);
+addSetting_DropDown('Hotbuild Preview UI','hotbuild_preview_display','UI',['ON','OFF'],0);
+addSetting_Text('Hotbuild Reset Time','hotbuild_reset_time','UI','Number',2000);
+addSetting_Text('Hotbuild Requeue Amount','hotbuild_requeue_amount','UI','Number',50);
