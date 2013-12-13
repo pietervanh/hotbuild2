6828dcbbb45adff6b84f1e2db43820b080828303
Pieter Vanhoutteghem <pietervanh@users.noreply.github.com>
2013-12-13 10:09:51 +0100

    Working Test Settings UI

--- settings/hotbuild_settings.js
+++ settings/hotbuild_settings.js
@@ -8,51 +8,81 @@ model.addSetting_Text('Hotbuild Requeue Amount','hotbuild_requeue_amount','UI','
 model.registerFrameSetting('hotbuild_info_frame', 'Hotbuild Preview', true);
 
 
+var hotbuildglobal = {};
+
+var hotbuilds1 = [
+        {displayname:"Vec Factory",json:"/pa/units/land/vehicle_factory/vehicle_factory.json"},
+        {displayname:"Bot Factory",json:"/pa/units/land/bot_factory/bot_factory.json"},
+        {displayname:"BotFabber",factory:"botfac",json:"/pa/units/land/fabrication_bot/fabrication_bot.json"},
+        {displayname:"Adv BotFabber",factory:"advbotfac",json:"/pa/units/land/fabrication_bot_adv/fabrication_bot_adv.json"},
+        {displayname:"VecFabber",factory:"vecfac",json:"/pa/units/land/fabrication_vehicle/fabrication_vehicle.json"},
+        {displayname:"Adv VecFabber",factory:"vecfac",json:"/pa/units/land/fabrication_vehicle_adv/fabrication_vehicle_adv.json"},
+
+];
+
+hotbuildglobal[hotbuild1] = hotbuilds1;
 
 function HotBuildSettingsViewModel()
 {
-    this.keyinfos = ko.observableArray([
-        {hbid:"hotbuild1",info:hotbuild1},
-        {hbid:"hotbuild2",info:hotbuild2},
-        {hbid:"hotbuild3",info:hotbuild3},
-        {hbid:"hotbuild4",info:hotbuild4},
-        {hbid:"hotbuild5",info:hotbuild5},
-        {hbid:"hotbuild6",info:hotbuild6},
-        {hbid:"hotbuild7",info:hotbuild7},
-        {hbid:"hotbuild8",info:hotbuild8},
-        {hbid:"hotbuild9",info:hotbuild9},
-        {hbid:"hotbuild10",info:hotbuild10}
+    var self = this;
+    self.keyinfos = ko.observableArray([
+        {hbid:"hotbuild1",info:hotbuildglobal[hotbuild1]}
     ]);
-    this.selectedhotbuild = ko.observableArray([""]);
+    self.selectedhotbuild = ko.observableArray([]);
     //this.buildings = ko.observableArray(hotbuildgamemodel.unitSpecs());
-    this.buildings = ko.observableArray(["Bot Factory","Vehicule Factory"]);
-    this.units = ko.observableArray(["Fabber","Dox","Ant"]);
-    this.selectedkeyinfo = ko.observable();
-    this.selectKey = function () {
-        this.selectedhotbuild(eval(this.selectedkeyinfo()));
+    self.buildings = ko.observableArray([
+                                         {displayname:"Bot Factory",json:"/pa/units/land/bot_factory/bot_factory.json"},
+                                         {displayname:"Vehicule Factory",json:"/pa/units/land/vehicle_factory/vehicle_factory.json"}
+                                        ]);
+    self.units = ko.observableArray([
+                                     {displayname:"BotFabber",factory:"botfac",json:"/pa/units/land/fabrication_bot/fabrication_bot.json"},
+                                     {displayname:"VecFabber",factory:"vecfac",json:"/pa/units/land/fabrication_vehicle/fabrication_vehicle.json"},
+                                     {displayname:"AA Bot",factory:"botfac",json:"/pa/units/land/bot_aa/bot_aa.json"},
+                                     {displayname:"Scout",facotry:"vecfac",json:"/pa/units/land/land_scout/land_scout.json"}
+                                    ]);
+    self.selectedkeyinfo = ko.observable();
+    self.selectKey = function () {
+        self.selectedhotbuild(eval(self.selectedkeyinfo()));
     };
     
-    this.selectedbuilding = ko.observable();
+    self.selectedbuilding = ko.observable();
     
-    this.selectedunit = ko.observable();
+    self.selectedunit = ko.observable();
     
     
 
-    this.addBuilding = function () {
-        this.selectedhotbuild.push(this.selectedbuilding());
+    self.addBuilding = function () {
+        self.selectedhotbuild.push(self.selectedbuilding());
     };
-    this.addUnit = function () {
-        this.selectedhotbuild.push(this.selectedunit());
+    self.addUnit = function () {
+        //if(self.selectedhotbuild contains already a unit with the same factory ignore)
+        var unitCheck = true;
+        for(var i = 0; i < self.selectedhotbuild().length; i++)
+        {
+          if(self.selectedhotbuild()[i].factory == self.selectedunit().factory)
+          {
+            unitCheck = false;
+            break;
+          } 
+        }
+        if(unitCheck)
+        {
+          self.selectedhotbuild.push(self.selectedunit());
+        }
     };
 
-    this.remFromList = function (item) {
-        this.selectedhotbuild.remove(item);
+    self.remFromList = function (item) {
+        self.selectedhotbuild.remove(item);
+    };
+    
+    self.Save = function ()
+    {
+      hotbuildglobal[eval(self.selectedkeyinfo())] =  self.selectedhotbuild();
     };
 
 }
 
 var hbuisettings = new HotBuildSettingsViewModel();
-
 
 $("#game_settings").children(":first").append("<li class='game_settings'>" +
                 "<a href='#tab_hotbuildprefs'>HOTBUILD</a>" +
@@ -60,17 +90,18 @@ $("#game_settings").children(":first").append("<li class='game_settings'>" +
 $("#game_settings").append('<div class="div_settings" id="tab_hotbuildprefs" data-bind="with: hbuisettings" style="height: 400px; overflow: scroll; overflow-x:hidden;">' +
                 '<h1>Work in Progress<h1/><br/>' +
                 'Select Hotbuild key: <select name="uihotbuildkey" data-bind="options: keyinfos, value: selectedkeyinfo, optionsText: \'hbid\', optionsValue: \'hbid\',optionsCaption: \'Select an key...\',click:$root.selectKey"></select><br/>' +
-                'Add Building to key: <select name="uihotbuildbuilding" data-bind="options: buildings, value: selectedbuilding ,optionsCaption: \'Select an Building...\'"></select>' +
+                'Add Building to key: <select name="uihotbuildbuilding" data-bind="options: buildings, value: selectedbuilding, optionsText:\'displayname\', optionsCaption: \'Select an Building...\'"></select>' +
                 '<button id="hbuiaddbuilding" type="submit" data-bind="click_sound: \'default\', rollover_sound: \'default\',click:$root.addBuilding">Add</button></br>' +
-                'Add Unit to key: <select name="uihotbuildunit" data-bind="options: units, value: selectedunit, optionsCaption: \'Select a unit...\'"></select>' +
+                'Add Unit to key: <select name="uihotbuildunit" data-bind="options: units, value: selectedunit, optionsText:\'displayname\', optionsCaption: \'Select a unit...\'"></select>' +
                 '<button id="hbuiaddunit" type="submit" data-bind="click_sound: \'default\', rollover_sound: \'default\',click:$root.addUnit">Add</button></br>' +
                 '<hr>' +
                 '<table cellspacing="2" cellpadding="2">' +
-                '<thead><tr><th>Building/Unit</th><th>Sequence</th></tr></thead>' +
+                '<thead><tr><th>Building/Unit</th><th>JSON</th><th>Sequence</th></tr></thead>' +
                 '<tbody data-bind="foreach: selectedhotbuild">' +
-                '<tr><td data-bind="text: $data"></td><td><button id="hbremovefromlist" type="submit" data-bind="click: $root.remFromList">Del</button></td></tr>' +
+                '<tr><td data-bind="text: displayname"></td><td data-bind="text: json"></td><td><button id="hbremovefromlist" type="submit" data-bind="click: $root.remFromList">Del</button></td></tr>' +
                 '</tbody>' +
                 '</table>' +
+                '<button id="hbuisave" type="submit" data-bind="click_sound: \'default\', rollover_sound: \'default\',click:$root.Save">Save</button>' +
             '</div>');
 
 ko.applyBindings(hbuisettings, $('#tab_hotbuildprefs')[0]);
