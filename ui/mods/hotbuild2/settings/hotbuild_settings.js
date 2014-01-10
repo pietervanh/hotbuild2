1f443ab378cfeb622de93fd97a9fc0a942453d13
Pieter Vanhoutteghem <pietervanh@users.noreply.github.com>
2014-01-10 11:52:59 +0100

    Add setting hotbuild_show_key_on_buildbar

--- ui/mods/hotbuild2/settings/hotbuild_settings.js
+++ ui/mods/hotbuild2/settings/hotbuild_settings.js
@@ -6,6 +6,7 @@
 //model.addSetting_DropDown('Hotbuild Preview UI','hotbuild_preview_display','UI',['ON','OFF'],0);
 model.addSetting_Text('Hotbuild Reset Time', 'hotbuild_reset_time', 'UI', 'Number', 2000);
 model.addSetting_Text('Hotbuild Requeue Amount', 'hotbuild_requeue_amount', 'UI', 'Number', 50);
+model.addSetting_DropDown('Hotbuild Show Key on BuildBar', 'hotbuild_show_key_on_buildbar', 'UI', ['ON','OFF'], 0);
 model.registerFrameSetting('hotbuild_info_frame', 'Hotbuild Preview', true);
 
 model.oldSettingsBeforeHotbuild = model.settings;
