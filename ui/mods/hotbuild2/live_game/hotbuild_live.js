e1ee1b24bc73320cbb803075e33c31a905e3bda8
pieter vanh <pieter.vanh@gmail.com>
2014-01-13 23:05:42 +0100

    fix for #15

--- ui/mods/hotbuild2/live_game/hotbuild_live.js
+++ ui/mods/hotbuild2/live_game/hotbuild_live.js
@@ -76,6 +76,53 @@ var hotbuild2live = (function () {
 	}
 	
 	
+	var keycodes = {
+	    37: "left",
+	    38: "up",
+	    39: "right",
+	    40: "down",
+	    45: "insert",
+	    46: "delete",
+	    8: "backspace",
+	    9: "tab",
+	    13: "enter",
+	    16: "shift",
+	    17: "ctrl",
+	    18: "alt",
+	    19: "pause",
+	    20: "capslock",
+	    27: "escape",
+	    32: "space",
+	    33: "pageup",
+	    34: "pagedown",
+	    35: "end",
+	    112: "f1",
+	    113: "f2",
+	    114: "f3",
+	    115: "f4",
+	    116: "f5",
+	    117: "f6",
+	    118: "f7",
+	    119: "f8",
+	    120: "f9",
+	    121: "f10",
+	    122: "f11",
+	    123: "f12",
+	    144: "numlock",
+	    145: "scrolllock",
+	    186: "semicolon",
+	    187: "equal",
+	    188: "comma",
+	    189: "dash",
+	    190: "period",
+	    191: "slash",
+	    192: "graveaccent",
+	    219: "openbracket",
+	    220: "backslash",
+	    221: "closebraket",
+	    222: "singlequote"
+	};
+
 	//fix for allowing multiple bindings per key
 	//for example stop = s / build mex = s
 	// stop = s = default mousetrap binding
@@ -84,8 +131,20 @@ var hotbuild2live = (function () {
 	
 	    if (!model.hasSelection() || model.showLanding() || model.chatSelected())
 	        return;
-	
-	    var value = String.fromCharCode(e.keyCode).toLowerCase();
+
+	    var value;
+	    console.log(e.keyCode);
+	    if (e.which >= 48 && e.which <= 90) {
+
+	        /* grab letters */
+	        value = String.fromCharCode(e.which).toLowerCase();
+	    } else {
+
+	        /* if not a letter look in key codes */
+	        value = keycodes[e.which];
+	    }
+
+	    console.log(value);
 	    for (var i = 1; i <= 20; i++) {
 	        if (hotbuildglobalkey["hotbuild" + i + "s"] == value) {
 	            hotbuild2.hotbuildManager.hotBuild(e, hotbuildglobal["hotbuild" + i + "s"]);
